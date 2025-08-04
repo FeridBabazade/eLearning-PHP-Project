1588e2a95815f660f41c1d288ffbb3a30bf59326
ferid <ferid-401@mail.ru>
2025-08-04 13:53:38 +0400

    navbar

--- config/app.php
+++ config/app.php
@@ -6,7 +6,7 @@ const APP_NAME = "Evo";
 const APP_VERSION = '1.0.0';
 const APP_AUTHOR = "Evo Team";
 const APP_DESCRIPTION = "A simple PHP application";
-const APP_URL = "http://localhost/eLearning-PHP-Project/";
+const APP_URL = "http://localhost/eLearning-PHP-Project";
 
 
 ?>
